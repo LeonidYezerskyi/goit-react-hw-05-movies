5eb5bd894fb8cd8d4667524b3568c801e7d8c00b
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-12-01 18:20:21 +0200

    start work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import React from 'react';
+import HomePage from '../pages/HomePage';
+import MoviesPage from '../pages/MoviesPage';
+import css from './App.module.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Movies from './Movies/Movies';
+import Home from './Home/Home';
+import cn from 'classnames';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div>
+      <nav className={css.header}>
+        <NavLink to='/home' className={({ isActive }) =>
+          cn(css.tabPage, { [css.active]: isActive, })}>
+          <Home />
+        </NavLink>
+        <NavLink to='/movies' className={({ isActive }) =>
+          cn(css.tabPage, { [css.active]: isActive, })}>
+          <Movies />
+        </NavLink>
+      </nav>
+
+      <>
+        <Routes >
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+        </Routes>
+      </>
+    </div >
   );
 };
